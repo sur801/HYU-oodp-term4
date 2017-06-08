b05de737823a71cbc23daedf9fa60e3e0eafe1ad
jhoon716 <jhoon716@gmail.com>
2017-06-08 21:52:41 +0900

    loading db implemented & use main template

--- flaskr/web_sever.py
+++ flaskr/web_sever.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main(name=None):
-    return render_template('person.html', name=name, person=getPerson())
+    return render_template('main.html', name=name, person=getPerson())
 
 @app.route('/call_hist')
 def call_hist():
