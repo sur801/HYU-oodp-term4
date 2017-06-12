dcc68bd763ae269dddcb51a5898fc047a25158bd
2016025496 <2016025496@hanyang.ac.kr>
2017-06-12 14:44:56 +0900

    front change

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -1,7 +1,12 @@
-function asking(person){
+function asking(id){
 
 	if (confirm("실행할래?")== true){    //확인
-		alert(person);
+		var xhr = new XMLHttpRequest();
+		var url = "http://localhost:5000/delete";
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader('Content-Type', 'text/plain');
+		xhr.send(id)
+
 	}else{   //취소
     	return;
 	}
